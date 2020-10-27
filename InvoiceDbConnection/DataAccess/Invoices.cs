b50ea4b4d1ba01634e74485d1e07b6d268b70243
Rolando Domínguez <rolando.fcfm2020@gmail.com>
2020-10-26 19:27:22 -0600

    Fist commit

---
+++ InvoiceDbConnection/DataAccess/Invoices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InvoiceDbConnection.DataAccess
+{
+    public partial class Invoices
+    {
+        public Invoices()
+        {
+            InvoiceProductItems = new HashSet<InvoiceProductItems>();
+        }
+
+        public Guid Id { get; set; }
+        public string Emisor { get; set; }
+        public Guid ClientId { get; set; }
+        public long Number { get; set; }
+        public string Series { get; set; }
+        public DateTime CreationDate { get; set; }
+        public decimal Total { get; set; }
+
+        public virtual Clients Client { get; set; }
+        public virtual ICollection<InvoiceProductItems> InvoiceProductItems { get; set; }
+    }
+}
---
+++ InvoiceDbConnection/Program.cs
@@ -0,0 +1,31 @@
+﻿using InvoiceDbConnection.DataAccess;
+using System;
+using System.Linq;
+
+namespace InvoiceDbConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RV_ERPContext DataContext = new RV_ERPContext();
+
+            var clients = DataContext.Clients.Select(s => new
+            {
+                s.Address,
+                s.CompanyName,
+                s.Id
+
+            }).ToList();
+
+            foreach (var c in clients)
+            {
+                Console.WriteLine("Nombre empresa: " + c.CompanyName);
+                Console.WriteLine("Id Empresa: " + c.Id);
+                Console.WriteLine("Dirección: " + c.Address);
+                Console.WriteLine("******************************************");
+            }
+        }
+    }
+}
